0849d0e705ad5977250f76d62182bc4a3bc2f6d6
Higinio01 <hrsp0001@ce.pucmm.edu.do>
2023-07-22 19:18:43 -0400

    Practica 1 terminada

--- practica-1/src/main/java/org/example/Main.java
+++ practica-1/src/main/java/org/example/Main.java
@@ -1,81 +1,72 @@
 package org.example;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Scanner;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class Main {
     public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
+        try (Scanner scanner = new Scanner(System.in)) {
             System.out.println("Ingrese la URL (Asegúrese de que incluya el protocolo HTTP/HTTPS)");
-            String url = sc.nextLine();
+            String urlStr = scanner.nextLine();
 
-            try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
-                HttpGet httpGet = new HttpGet(url);
-                HttpResponse httpResponse = httpClient.execute(httpGet);
+            try {
+                URL url = new URL(urlStr);
+                HttpClient httpClient = HttpClient.newHttpClient();
+                HttpRequest httpRequest = HttpRequest.newBuilder(URI.create(urlStr)).build();
+                HttpResponse<Void> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.discarding());
 
-                String contentType = httpResponse.getEntity().getContentType().getValue();
+                String contentType = httpResponse.headers().firstValue("Content-Type").orElse("Tipo del recurso no encontrado");
                 System.out.println("Tipo de recurso: " + contentType);
 
-                if (contentType.equalsIgnoreCase("text/html; charset=utf-8")) {
-                    Document doc = Jsoup.connect(url).get();
+                if (contentType.contains("text/html")) {
+                    Document doc = Jsoup.connect(urlStr).get();
                     String html = doc.html();
-                    System.out.println("Cantidad de líneas: " + html.split("\n").length);
+                    System.out.println("Cantidad de líneas: " + html.lines().count());
 
                     Elements paragraphs = doc.select("p");
                     System.out.println("Cantidad de párrafos (p): " + paragraphs.size());
 
-                    int imageCount = 0;
-                    for (Element paragraph : paragraphs) {
-                        imageCount += paragraph.select("img").size();
-                    }
+                    long imageCount = paragraphs.stream().mapToLong(paragraph -> paragraph.select("img").size()).sum();
                     System.out.println("Cantidad de imágenes en los párrafos: " + imageCount);
 
                     Elements forms = doc.select("form");
                     System.out.println("Cantidad de formularios (form): " + forms.size());
 
-                    int postCount = 0;
-                    int getCount = 0;
-                    for (Element form : forms) {
-                        String method = form.attr("method");
-                        if (method.equalsIgnoreCase("post")) {
-                            postCount++;
-                        } else if (method.equalsIgnoreCase("get")) {
-                            getCount++;
-                        }
-                    }
+                    long postCount = forms.stream().filter(form -> form.attr("method").equalsIgnoreCase("post")).count();
+                    long getCount = forms.stream().filter(form -> form.attr("method").equalsIgnoreCase("get")).count();
                     System.out.println("Cantidad de formularios por método:");
                     System.out.println("POST: " + postCount);
                     System.out.println("GET: " + getCount);
 
-                    for (Element form : forms) {
+                    forms.forEach(form -> {
                         System.out.println("Campos de formulario:");
-                        Elements inputs = form.select("input");
-                        for (Element input : inputs) {
-                            System.out.println("Tipo: " + input.attr("type"));
-                        }
-                    }
+                        form.select("input").forEach(input -> System.out.println("Tipo: " + input.attr("type")));
+                    });
 
-                    for (Element form : forms) {
-                        String method = form.attr("method");
-                        if (method.equalsIgnoreCase("post")) {
-                            Document response = Jsoup.connect(url)
-                                    .data("asignatura", "practica1")
-                                    .header("matricula-id", "matricula o id asignado")
-                                    .post();
-                            System.out.println("Respuesta de la petición POST: " + response.body().text());
-                        }
-                    }
+                    forms.stream()
+                            .filter(form -> form.attr("method").equalsIgnoreCase("post"))
+                            .forEach(form -> {
+                                try {
+                                    Document response = Jsoup.connect(urlStr)
+                                            .data("asignatura", "practica1")
+                                            .header("matricula-id", "matricula o id asignado")
+                                            .post();
+                                    System.out.println("Respuesta de la petición POST: " + response.body().text());
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            });
                 }
-            } catch (IOException e) {
+            } catch (IOException | InterruptedException e) {
                 e.printStackTrace();
             }
         }
